dbcdabc9517e13196aca550c136fb580fa486bc6
Hector Montillo <hector.montillo@utp.edu.co>
2020-03-14 09:27:05 -0500

    Limpieza de las carpetas del proyecto

--- clientfolder/client.py
+++ clientfolder/client.py
@@ -1,109 +1,210 @@
- 
 import zmq
 import hashlib 
 import sys
-context = zmq.Context()
+import os
 
 PS = 1024 * 1024
 
-#  SocketProxi  to talk to server
-socketProxi = context.socket(zmq.REQ)
-socketProxi.connect("tcp://localhost:5556")
+# Encode and Decode data
+class Code:
+	def __init__(self,f):
+		self.format = f
+	def deco(self, data):
+		return data.decode(self.format)
+	def enco(self, data):
+		return data.encode(self.format)
 
-def deco(data, format='ascii'):
-    return data.decode(format)
-def enco(data, format='ascii'):
-    return data.encode(format)
-
-def compareHash(hash1, hash2):
-    return True if hash1 == hash2 else False
-
-def getHash(file):
-    return hashlib.sha256(file).hexdigest()
-
-def checkIntegrity(file, hash):
-    filehash = hashlib.sha256(file).hexdigest()
-    print("Hash from file:\t\t"+filehash)
-    print("Hash from server:\t"+hash)
-    print("Segment Succefully" if compareHash(filehash, hash) else "An error has occurred, retry the operation")
-
-def upload(file):
-    with open(file, 'rb') as f:
-        parts = list()
-        sha256 = hashlib.sha256()
-        while True:
-            segment = f.read(PS)
-            if not segment:
-                hashCompleto = sha256.hexdigest()
-                socketProxi.send_multipart([b'upload', enco(file), enco(hashCompleto) ]+parts)
-                balance = socketProxi.recv_json()
-                distributed_upload(file, balance)
-                break
-            sha256.update(segment)
-            parts.append(enco(getHash(segment)))
-
-def send_request(address, req):
-    socket = context.socket(zmq.REQ)
-    socket.connect("tcp://"+address)
-    socket.send_multipart(req)
-    message = socket.recv_multipart()
-    socket.close()
-    return message
-    
-def select_server(balance, segment_hash):
-    for key in balance.keys():
-        if segment_hash in balance[key]:
-            return key
-    return "error"
-
-def distributed_upload(file, balance):
-    with open(file, 'rb') as f:
-        parts = 0
-        while True:
-            segment = f.read(PS)
-            if not segment:
-                print("Has been upload " + str(parts) + " parts")
-                break
-            server = select_server(balance, getHash(segment))
-            if server=='error':
-                print("An error has been occur!!")
-            else:
-                message = send_request(server,[b'upload',segment])
-                print("Resutl for part " + str(parts))
-                checkIntegrity(segment,deco(message[0])) 
-                parts+=1
-  
-def download(file):
-    socketProxi.send_multipart([b"download",file.encode('ascii')])
-    response = socketProxi.recv_json()
-    if "error" in response.keys():
-        print(response["error"])
-    else:
-        distributed_download(file, response)
-
-def distributed_download(file, response):
-    newfile = open(file, "ab")
-    sha256 = hashlib.sha256()
-    for h,s in zip(response["parts"],response["server"]):
-        res = send_request(s,[b"download", enco(h)])
-        sha256.update(res[0])
-        newfile.write(res[0])
-    newfile.close()
-    print("Hash from download:\t"+sha256.hexdigest())
-    print("Hash from proxi:\t"+response["hash"])
-
-def listing():
-    socketProxi.send_multipart([b"list"])
-    message = socketProxi.recv_multipart()
-    print("Your files : ")
-    for i in message:
-        print(deco(i))  
-
-if sys.argv[1] == 'upload':
-    upload(sys.argv[2])
-elif sys.argv[1] == 'list':
-    listing()
-elif sys.argv[1] == 'download':
-    download(sys.argv[2])
+# Get sha hash 
+class Sha:
+	def __init__(self, v):
+		self.version = v
+	def getHash(self, file):
+		if (self.version == '1'):
+			return hashlib.sha1(file).hexdigest()
+		if (self.version == '256'):
+			return hashlib.sha256(file).hexdigest()
 
 
+class Chord_Client:
+	def __init__(self):
+		self.context = zmq.Context()
+		#Utilidades
+		self.coder = Code('ascii')
+		self.hasher = Sha(256)
+
+	def run(self):
+		
+		try:
+			command = sys.argv[1]
+		except:
+			print('You must use an action: upload or download')
+
+		if command == 'state':
+			self.state(sys.argv[2])
+		elif command == 'upload':
+			self.upload(sys.argv[2], sys.argv[3])
+		elif command == 'download':
+			self.download(sys.argv[2], sys.argv[3])
+
+
+	def send_request(self, req, address, send='multipart', recv='multipart'):
+		socket = self.context.socket(zmq.REQ)
+		socket.connect("tcp://"+address)
+
+		if(send=='multipart'):
+			socket.send_multipart(req)
+		elif(send=='json'):
+			socket.send_json(req)
+
+		if(recv=='multipart'):
+			res = socket.recv_multipart()
+		elif(recv=='json'):
+			res = socket.recv_json()
+
+		socket.close()
+		return res
+
+	def upload_segment(self,name_file, segment, server_address, save= True):
+		name_segment = hashlib.sha1(segment).hexdigest()
+		#print(name_segment, type(name_segment))
+		request = [b'upload',self.coder.enco(name_segment),segment]
+		address = server_address
+		while True:
+			response = self.send_request(request,address)
+			if self.coder.deco(response[0]) == 'success upload':
+				print("Segment was upload: {} on server {}".format(name_segment,address))
+				if save:
+					f = open(name_file+".chord", "a")
+					f.write(name_segment+"\n")
+					f.close()
+				return True
+			else:
+				address = self.coder.deco(response[1])
+				if address == server_address:
+					return False
+
+	def finish_chord_file(self, name_file, complete_hash):
+		f = open(name_file+".chord", "r")
+		lines = f.readlines()
+		lines.insert(0,complete_hash+'\n')
+		lines.insert(0,name_file+'\n')
+		f.close()
+
+		f = open(name_file+".chord", "w")
+		f.writelines(lines)
+		f.close()
+
+	def upload_chord_file(self, name_file, server_address):
+		with open(name_file, 'rb') as f:
+			chord_data = f.read()
+			hash_chord_file = hashlib.sha1(chord_data).hexdigest()
+			res = self.upload_segment(hash_chord_file,chord_data,server_address,save=False)
+			if res:
+				print('Magnet link is already!: '+ hash_chord_file)
+			else:
+				print('An error has occurred trying to generate magnet link')
+
+
+	def upload(self, name_file, server_address):
+		with open(name_file, 'rb') as f:
+			sha1 = hashlib.sha1()
+			while True:
+				segment = f.read(PS)
+				if not segment:
+					self.finish_chord_file(name_file,	sha1.hexdigest())
+					self.upload_chord_file(name_file+".chord",server_address)
+					print("The file was upload!")
+					break
+				sha1.update(segment)
+				res = self.upload_segment(name_file,segment, server_address)
+				if not res:
+					print("The upload was canceled")
+					break
+
+	def download_segment(self,name_file,name_segment,server_address):
+		request = [b'download',self.coder.enco(name_segment)]
+		address = server_address
+		while True:
+			response = self.send_request(request,address)
+			if self.coder.deco(response[0]) == 'success download':
+				with open(name_file, 'ab') as f:
+					f.write(response[1])
+				return True
+			elif self.coder.deco(response[0]) == 'file not found error':
+				print("Segment not found!")
+				return False
+			else:
+				address = self.coder.deco(response[1])
+				if address == server_address:
+					print("Hash out of limits!")
+					return False
+
+	def clear_file(self,name_file):
+		f = open(name_file, 'w')
+		f.write('')
+		f.close()
+
+	def download(self,chord_file, server_address):
+		if '.chord' in chord_file:
+			with open(chord_file, 'r') as f:
+				lines = f.readlines()
+				name_file = lines.pop(0)[:-1]
+				complete_hash = lines.pop(0)[:-1]
+				cont = 0
+				self.clear_file(name_file)
+				for name_segment in lines:
+					name_segment = name_segment[:-1]
+					print(cont, name_segment)
+					cont += 1
+					res = self.download_segment(name_file,name_segment,server_address)
+					if not res:
+						print('Donwload was canceled, an error has occurred')
+						break
+				self.check_integrity(complete_hash, name_file)
+				print('Donwload finished!')
+		else:
+			if len(chord_file) == 40:
+				print("Downloading with magnet link!")
+				res = self.download_segment('temp.chord',chord_file,server_address)
+				if not res:
+					print("Magnet link invalid!")
+				else:
+					self.download('temp.chord',server_address)
+					os.remove('temp.chord')
+			else:
+				print("Cord file or magnet link invalid")
+
+	
+	def check_integrity(self,complete_hash, name_file):
+		with open(name_file, "rb") as f:
+			sha1 = hashlib.sha1()
+			while True:
+				segment = f.read(PS)
+				if not segment:
+					if sha1.hexdigest() == complete_hash:
+						print("Succefully download: \nHash registered:\t{}\nHash download:\t{}".format(complete_hash,sha1.hexdigest()))
+					else:
+						print("Failure download: File corrupted!")
+					break
+				sha1.update(segment)
+
+	def state(self, address):
+		request = [b'state']
+		intial_address = address
+		current_address = address
+		while(True):
+			response = self.send_request(request,current_address,recv='json')
+			print("Chord State--------->")
+			print("Name: "+response['name'])
+			print("Address: "+response['address'])
+			print("Lim: "+', '.join(list(map(str,response['lim']))))
+			print("Successor: "+response['successor'])
+			print("Predecessor: "+response['predecessor'])
+			print("-------------------->")
+			current_address = response['successor']
+			if(intial_address == current_address):
+				break
+
+if __name__ == "__main__":
+	client = Chord_Client()
+	client.run()
--- serverfolder/server.py
+++ serverfolder/server.py
@@ -2,64 +2,271 @@ import zmq
 import sys
 import hashlib 
 import os
+import random
 
-# ip capacity proxiadd
-ip = sys.argv[1].split(':')
-capacity = sys.argv[2]
-proxiAddress = sys.argv[3]
+LIMIT = (2**160)-1
 
+# Encode and Decode data
+class Code:
+	def __init__(self,f):
+		self.format = f
+	def deco(self, data):
+		return data.decode(self.format)
+	def enco(self, data):
+		return data.encode(self.format)
 
-address = "{}:{}".format(ip[0],ip[1])
+# Get sha hash 
+class Sha:
+	def __init__(self, v):
+		self.version = v
+	def getHash(self, file):
+		if (self.version == '1'):
+			return hashlib.sha1(file).hexdigest()
+		if (self.version == '256'):
+			return hashlib.sha256(file).hexdigest()
 
-# Socket for clients
-context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.bind("tcp://*:{}".format(ip[1]))
+#Cord Server
+class Chord_Server:
+	def __init__(self,address,connect):
+		# Utilidades
+		self.coder = Code('ascii')
+		self.naming = Sha('1')
+		# Atributos
+		self.name = self.generate_name(40)
+		self.address = address
+		self.lim = [self.name+1,LIMIT,0,self.name] if connect=='genesis' else None
+		#ZMQ
+		#General Socket for listen Request
+		self.context = zmq.Context()
+		self.socket = self.context.socket(zmq.REP)
+		self.socket.bind("tcp://*:{}".format(self.address.split(':')[1]))
+		#Socket to send request to server successor and predecessor
+		self.successor_server_socket = None
+		self.successor_server_address = connect
+		self.predecessor_server_socket = None
+		self.predecessor_server_address = address
 
+	def loop(self):
+		while True:
+			request = self.socket.recv_multipart()
+			self.log(request)
+			requestAction = self.coder.deco(request[0])
+			if requestAction == 'join':
+				self.join_server(int(self.coder.deco(request[1])),self.coder.deco(request[2]))
+			elif requestAction == 'left successor':
+				self.left_to_ring(self.deco_list(request))
+			elif requestAction == 'left predecessor':
+				self.left_predecessor(self.coder.deco(request[1]))
+			elif requestAction == 'successor':
+				self.successor_update(self.coder.deco(request[1]))
+			elif requestAction == 'upload':
+				self.upload(self.coder.deco(request[1]),request[2])
+			elif requestAction == 'download':
+				self.download(self.coder.deco(request[1]))
+			elif requestAction == 'list':
+				pass
+			elif requestAction == 'state':
+				self.state()
 
-# Socket for proxi
-socketProxi = context.socket(zmq.REQ)
-socketProxi.connect("tcp://{}".format(proxiAddress))
-socketProxi.send_multipart([b"addserver", capacity.encode('ascii'), address.encode('ascii')])
-response = socketProxi.recv()
-socketProxi.close()
-print(response.decode('ascii'))
+	def run(self):
+		if self.successor_server_address == 'genesis':
+			print("Genesis server has been initialized!!")
+			print("Name: {}, Address: {}".format(self.name, self.address))
+			self.successor_server_address = self.address
+		else:
+			self.join_to_ring()
+		self.loop()
+	
+	def down(self):
+		if (self.successor_server_address == self.address):
+			print("Ultimo servidor del anillo apagado!")
+		else:
+			print("Servidor Apagado")
+			request = [b'left successor', self.coder.enco(self.predecessor_server_address)]+self.enco_list(self.lim[:-1])
+			response = self.successor_send_request(request)
+			self.log(response,'Response successor')
 
-def deco(data, format='ascii'):
-	return data.decode(format)
-def enco(data, format='ascii'):
-	return data.encode(format)
-def getHash(file):
-	return hashlib.sha256(file).hexdigest()
+			request = [b'left predecessor', self.coder.enco(self.successor_server_address)]
+			response = self.predecessor_send_request(request)
+			self.log(response, 'Response predecessor')
 
-def create_directory(name):
+	def generate_name(self,length):
+		return int(self.naming.getHash(self.coder.enco(self.generate_random_string(40))),16)
+
+	def generate_random_string(self,length):
+		string = ""
+		for i in range(length):
+			char = random.randint(0,127)
+			string+=chr(char)
+		return string
+	
+	def successor_send_request(self,req):
+		self.successor_server_socket = self.context.socket(zmq.REQ)
+		self.successor_server_socket.connect("tcp://{}".format(self.successor_server_address))
+		self.successor_server_socket.send_multipart(req)
+		response = self.successor_server_socket.recv_multipart()
+		self.successor_server_socket.close()
+		return response
+
+	def predecessor_send_request(self,req):
+		self.predecessor_server_socket = self.context.socket(zmq.REQ)
+		self.predecessor_server_socket.connect("tcp://{}".format(self.predecessor_server_address))
+		self.predecessor_server_socket.send_multipart(req)
+		response = self.predecessor_server_socket.recv_multipart()
+		self.predecessor_server_socket.close()
+		return response
+
+	def left_to_ring(self, request):
+		self.predecessor_server_address = request[1]
+		newlim = request[2:]+self.lim[1:]
+		self.lim = list(map(int,newlim))
+		print("My lim: ")
+		print(self.lim)
+		self.socket.send_multipart([b'left success'])
+
+	def left_predecessor(self,successor_server_address):
+		self.successor_server_address = successor_server_address
+		self.socket.send_multipart([b'left success'])
+			
+	def join_to_ring(self):
+		request = [b"join", self.encode_name(), self.coder.enco(self.address)]
+		response = self.successor_send_request(request)
+		self.log(response,'Response Successor')
+		if(self.coder.deco(response[0]) == 'success'):
+			print("Server has been initialized!!")
+			print("Name: {}, Address: {}".format(self.name, self.address))
+			self.lim = list(map(self.deco_and_int, response[1:-1]))
+			self.predecessor_server_address = self.coder.deco(response[-1])
+			self.notify_successor()
+		else:
+			self.successor_server_address = self.coder.deco(response[1])
+			self.join_to_ring()
+		
+	def join_server(self,server_num, server_address):
+		if (server_num >= self.lim[0] and server_num <= self.lim[1]):
+			res_lim = [self.coder.enco(str(self.lim[0])),self.coder.enco(str(server_num)), self.coder.enco(self.predecessor_server_address)]
+			self.socket.send_multipart([b"success"]+res_lim)
+			self.lim[0] = server_num+1
+			self.predecessor_server_address = server_address
+		else:
+			try:
+				if (server_num >= self.lim[2] and server_num <= self.lim[3]):
+					res_lim = [self.coder.enco(str(self.lim[0])), self.coder.enco(str(self.lim[1])), self.coder.enco(str(self.lim[2])), self.coder.enco(str(server_num)), self.coder.enco(self.predecessor_server_address)]
+					self.socket.send_multipart([b"success"]+res_lim)
+					self.lim = [server_num+1, self.name]
+					self.predecessor_server_address = server_address
+				else:
+					self.socket.send_multipart([b"rejoin", self.coder.enco(self.successor_server_address)])
+			except IndexError:
+				self.socket.send_multipart([b"rejoin", self.coder.enco(self.successor_server_address)])
+				
+		print("My lim: ")
+		print(self.lim)
+		
+	def check_segment(self,name_segment):
+		number_segment = int(name_segment,16)
+		if (number_segment >= self.lim[0] and number_segment <= self.lim[1]):
+			return True
+		else:
+			try:
+				if (number_segment >= self.lim[2] and number_segment <= self.lim[3]):
+					return True
+				else:
+					return False
+			except IndexError:
+				return False
+
+	def rejoin_server(self,address):
+		self.successor_server_address = address
+		self.join_to_ring()
+
+	def notify_successor(self):
+		request = [b"successor", self.coder.enco(self.address)]
+		response = self.predecessor_send_request(request)
+		self.log(response,'Response Predecessor')
+
+	def successor_update(self, address):
+		self.successor_server_address = address
+		self.socket.send_multipart([b'success update'])
+
+	def create_segment(self, name_segment, segment):
+		self.create_directory()
+		with open("{}/{}".format(self.address,name_segment), "wb") as f:
+			f.write(segment)
+
+	def upload(self, name_segment, segment):
+		if (self.check_segment(name_segment)):
+			self.create_segment(name_segment, segment)
+			self.socket.send_multipart([b'success upload'])
+		else:
+			self.socket.send_multipart([b'failure upload', self.coder.enco(self.successor_server_address)])
+
+	def download(self, name_segment):
+		if (self.check_segment(name_segment)):
+			try:
+				with open("{}/{}".format(self.address,name_segment), "rb") as f:
+					segment = f.read()
+					self.socket.send_multipart([b'success download',segment])
+			except FileNotFoundError:
+				self.socket.send_multipart([b'file not found error'])
+		else:
+			self.socket.send_multipart([b'failure download', self.coder.enco(self.successor_server_address)])
+
+		'''
+		try:
+			print(name_segment)
+			with open("{}/{}".format(self.address,name_segment), "rb") as f:
+				segment = f.read()
+				self.socket.send_multipart([b'success',segment])
+		except FileNotFoundError:
+	
+			self.socket.send_multipart([b'file not found error'])
+		'''
+	def state(self):
+		state = {
+			'name': str(self.name),
+			'address': self.address,
+			'lim': self.lim,
+			'successor': self.successor_server_address,
+			'predecessor': self.predecessor_server_address
+		}
+		self.socket.send_json(state)
+
+	def deco_and_int(self, data):
+		return int(self.coder.deco(data))
+
+	def enco_list(self, lista):
+		return list(map(self.coder.enco,map(str, lista)))
+
+	def deco_list(self, lista):
+		return list(map(self.coder.deco,lista))
+
+	def create_directory(self):
+		try:
+			os.mkdir(self.address)
+			print("Directory " , self.address ,  " Created ") 
+		except FileExistsError:
+			print("Using", self.address, "directory")
+
+	def encode_name(self,i=0):
+		return self.coder.enco(str(self.name+i))
+	
+	def log(self,req, att='General Socket'):
+		print("{}: {}".format(self.coder.deco(req[0]),att))
+		for i in req[1:]:
+			try:
+				print("--> "+self.coder.deco(i))
+			except:
+				print("--> Binary data")
+
+if __name__ == "__main__":
+	server_address = sys.argv[1]
 	try:
-		os.mkdir(name)
-		print("Directory " , name ,  " Created ") 
-	except FileExistsError:
-		print("Directory " , name ,  " already exists")
-
-def upload(file):
-	name = getHash(file)
-	create_directory(address)
-	newfile = open("{}/{}".format(address,name), "wb")
-	newfile.write(file)
-	newfile.close()
-	socket.send_multipart([enco(name)])
-
-def download(hashfile):
-	with open(address+"/"+hashfile, "rb") as file:
-		socket.send_multipart([file.read()])
-
-def listing():
-	pass
-
-while True:
-	request = socket.recv_multipart()
-	requestAction = deco(request[0])
-	if requestAction == 'upload':
-		upload(request[1])
-	elif requestAction == 'download':
-		download(deco(request[1]))
-	elif requestAction == 'list':
-		listing()
+		successor_server_address = sys.argv[2]
+	except:
+		successor_server_address = 'genesis'
+	server = Chord_Server(server_address,successor_server_address)
+	try:
+		server.run()
+	except KeyboardInterrupt:
+		server.down()
